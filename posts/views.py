1bb1cc4c642ef4edf34ebeabf3ce440e315cc3db
sohyun lee <leeee_sh@naver.com>
2021-03-23 16:24:51 +0900

    post, video Read 구현 & post Create 일부 구현

--- posts/views.py
+++ posts/views.py
@@ -8,23 +8,34 @@ def posts(request):
     context = {
         'posts' :  posts
     }
-    return render(request, 'posts/posts.html', context=context)
+    return render(request, 'posts/posts.html', context)
+
+def new(request):
+    return render(request, 'posts/new.html')
 
 def create(request):
     return render(request, 'posts/create.html')
 
-def detail(request):
-    return render(request, 'posts/detail.html')
+def detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {
+        'post' :  post
+    }
+    return render(request, 'posts/detail.html', context)
 
 def video(request):
     videos = Video.objects.all()
     context = {
         'videos' :  videos
     }
-    return render(request, 'posts/video.html', context=context)
+    return render(request, 'posts/video.html', context)
 
-def videodetail(request):
-    return render(request, 'posts/video_detail.html')
+def videodetail(request, video_id):
+    video = Video.objects.get(id=video_id)
+    context = {
+        'video' :  video
+    }
+    return render(request, 'posts/video_detail.html', context)
 
 def signin(request):
     return render(request, 'posts/signin.html')
