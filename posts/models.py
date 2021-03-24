418680ddc52e8323190cc82c5eda9df382e2d957
sohyun lee <leeee_sh@naver.com>
2021-03-24 18:05:13 +0900

    author 필드 제거 & user 필드 생성

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
 
-    author = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     body = models.TextField()
     created_at = models.DateTimeField()
 
@@ -18,4 +19,7 @@ class Video(models.Model):
     created_at = models.DateTimeField()
 
     def __str__(self):
-        return f'{self.author}: {self.body}'
+        if self.user:
+            return f'{self.user.get_username()}: {self.body}'
+        else:
+            return f'{self.body}'
