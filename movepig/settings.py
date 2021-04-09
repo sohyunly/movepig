5a2dc74993bbfebfaf8b2c37ded5a6c7c2862dec
sohyun lee <leeee_sh@naver.com>
2021-04-09 15:45:34 +0900

    settings 수정(RDS 인스턴스)

--- movepig/settings.py
+++ movepig/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'movepig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movepig',
+        'USER': 'celine',
+        'PASSWORD': 'likelion123',
+        'HOST': 'movepig.c06tmjzh9kbw.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
